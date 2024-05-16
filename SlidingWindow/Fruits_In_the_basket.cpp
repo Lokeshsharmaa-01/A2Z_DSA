1bb9448d3eb29c4903f9f43b8dfea26c05b2e99c
Lokeshsharmaa-01 <iamlokesh2024@gmail.com>
2024-05-16 22:56:45 +0530

    adding better code for fruit in the bascket

--- SlidingWindow/Fruits_In_the_basket.cpp
+++ SlidingWindow/Fruits_In_the_basket.cpp
@@ -1,6 +1,6 @@
 // Brute force for this question is simple generate all possible subarray and find max length
 
-
+// T.C -> N X N
 
 int maxlength(int arr[]){
     int n = 6;
@@ -15,4 +15,39 @@ int maxlength(int arr[]){
         }
     }
     return maxlen;
+}
+
+
+// Better approch 
+
+// <---------------- SLIDING WINDOW AND TWO POINTER --------------------->
+
+// in this approch first we put element in map data structure for count occurens
+// then we check if it's size is greater then 2 then we trim window from left
+// if size is less then or equal to 2 we count the length 
+// and at last we return the max len
+
+int maxlength(int arr[]){
+    int l = 0;
+    int r = 0;
+    unordered_map<int,int> mpp;
+    while (r<n)
+    {
+        mpp[arr[r]]++;
+        if (mpp.size() > 2)
+        {
+            while(mpp.size() > 2){
+                mpp[arr[i]]--;
+                if(mpp[arr[l]] == 0) mpp.erase(arr[l]);
+                l++;
+            }
+        }
+        if(mpp.size() <= 2){
+            maxlen = max(maxlen, r-l+1);
+        }
+        r++;
+        
+    }
+    return maxlen;
+    
 }
