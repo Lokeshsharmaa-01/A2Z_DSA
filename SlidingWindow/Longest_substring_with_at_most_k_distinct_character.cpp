1293af523d24e9a2cfd039b172b86ad42984e9b5
Lokeshsharmaa-01 <iamlokesh2024@gmail.com>
2024-05-18 22:35:38 +0530

    optimal approch for last commit

--- SlidingWindow/Longest_substring_with_at_most_k_distinct_character.cpp
+++ SlidingWindow/Longest_substring_with_at_most_k_distinct_character.cpp
@@ -7,7 +7,7 @@
         int maxlen = 0;
         unordered_map<char,int> mpp;
         for(int i= 0; i<s.size(); i++){
-            mpp.clear(); // clear map every time
+            mpp.clear(); // clear map in every itration
             for(int j = i ; j<s.size(); j++){
                 mpp[s[j]]++;
                 if(mpp.size() <= k){
@@ -17,3 +17,27 @@
         }
         return maxlen;
  }
+
+
+ // Optimal/Better
+ // in this approch we trim down all left similar character
+
+int longestKSubstr(string s, int k){
+    int maxlen = 0;
+    int left =0, right = 0;
+    unordered_map<char,int> mpp;
+    while(right < s.size()){
+        mpp[s[right]]++;
+        while (mpp.size() > k) // for optimal we use if insted of while
+        {
+            mpp[s[left]]--;
+            if(mpp.size() == 0) mpp.clear(mpp[s[left]]);
+            left++;
+        }
+        if(mpp.size() <= k){
+            maxlen = max(maxlen, right-left+1);
+        }
+        
+    }
+    return maxlen;
+}
