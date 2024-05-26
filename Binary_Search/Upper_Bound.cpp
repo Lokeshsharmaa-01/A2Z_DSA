bcb6bc521bbc681fb4d6907abf9d7b73626b425b
Lokeshsharmaa-01 <iamlokesh2024@gmail.com>
2024-05-26 21:30:49 +0530

    upper bound

--- Binary_Search/Upper_Bound.cpp
+++ Binary_Search/Upper_Bound.cpp
@@ -1,15 +1,12 @@
-// simple binary search
-
-
-int lowerBound(int arr[], int n, int x){
+int upperbound(int arr[], int n , int x){
     int low = 0;
     int high = n - 1;
     int ans = n;
     while(low <= high){
         int mid = low + (high - low)/2;
-        if(arr[mid] >= x ){
+        if(arr[mid] > x){
             ans = mid;
-            high = mid - 1;
+            high = mid -1;
         }else{
             low = mid + 1;
         }
